a404384538152f14a10bc22f766a46b131dd69fe
MysticBlueTopaz <mysticBlueTopaz@gmail.com>
2025-12-18 20:42:52 -0500

    Added My Intro

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,8 @@
 # Hello AI/ML Bootcamp!
 # Edit the variables below to introduce yourself
 
-name = "Your Name Here"
-fun_fact = "One fun fact about me"
+name = "Erika Wooldridge"
+fun_fact = "I have remodeled 6 houses"
 
 # Don't change anything below this line
 print(f"Hello! I'm {name}.")
